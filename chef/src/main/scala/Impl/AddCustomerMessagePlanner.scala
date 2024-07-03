eca41f8ce4aac0ef8a909915e7c9310bde955a53
EnderXie23 <3142007828@qq.com>
2024-07-03 23:11:28 +0800

    Chef may read orders

--- chef/src/main/scala/Impl/AddCustomerMessagePlanner.scala
+++ chef/src/main/scala/Impl/AddCustomerMessagePlanner.scala
@@ -4,15 +4,38 @@ import Common.API.{PlanContext, Planner}
 import Common.DBAPI.{startTransaction, writeDB}
 import cats.effect.IO
 import io.circe.generic.auto.*
-
 import Common.API.{PlanContext, Planner}
 import Common.DBAPI.*
 import Common.Object.{ParameterList, SqlParameter}
 import Common.ServiceUtils.schemaName
 import APIs.CustomerAPI.CustomerQueryMessage
 import cats.effect.IO
+import cats.syntax.all.toTraverseOps
+import io.circe.Json
 import io.circe.generic.auto.*
 
+case class ChefReadOrdersPlanner(chefName: String, override val planContext: PlanContext) extends Planner[List[(String, String, Int)]] {
+  override def plan(using PlanContext): IO[List[(String, String, Int)]] = {
+    // Define the SQL query to fetch orders
+    val sqlQuery = s"SELECT customer_name, dish_name, order_count FROM ${schemaName}.order_rec"
+
+    // Execute the query and get the results as JSON
+    val jsonResultsIO: IO[List[Json]] = readDBRows(sqlQuery, List.empty)
+
+    // Convert the JSON results to a list of tuples
+    jsonResultsIO.flatMap { jsonResults =>
+      IO.fromEither {
+        jsonResults.traverse { json =>
+          for {
+            customerName <- json.hcursor.get[String]("customer_name")
+            dishName <- json.hcursor.get[String]("dish_name")
+            orderCount <- json.hcursor.get[Int]("order_count")
+          } yield (customerName, dishName, orderCount)
+        }
+      }
+    }
+  }
+}
 
 case class AddCustomerMessagePlanner(chefName: String, customerName: String, override val planContext: PlanContext) extends Planner[String]:
   override def plan(using PlanContext): IO[String] = {
--- chef/src/main/scala/Process/Routes.scala
+++ chef/src/main/scala/Process/Routes.scala
@@ -14,6 +14,11 @@ import org.http4s.dsl.io.*
 object Routes:
   private def executePlan(messageType:String, str: String): IO[String]=
     messageType match {
+      case "ChefReadOrders" =>
+        IO(decode[ChefReadOrdersPlanner](str).getOrElse(throw new Exception("Invalid JSON for ChefReadOrders")))
+          .flatMap{m=>
+            m.fullPlan.map(_.asJson.toString)
+          }
       case "AddCustomerMessage" =>
         IO(decode[AddCustomerMessagePlanner](str).getOrElse(throw new Exception("Invalid JSON for AddCustomerMessage")))
            .flatMap{m=>
