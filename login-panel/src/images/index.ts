2547ac4a204586a1af163056c1abc67c2c04fa2e
Quan Gu <cooolquan@163.com>
2024-07-08 13:31:37 +0800

    realize picture showing

---
+++ login-panel/src/images/index.ts
@@ -0,0 +1,5 @@
+export {default as fuxuan} from '../images/fuxuan.jpg';
+export {default as caesar_salad} from '../images/caesar_salad.jpg';
+export {default as margherita_pizza} from '../images/margherita_pizza.jpg';
+export {default as spaghetti_carbonara} from '../images/spaghetti_carbonara.jpg';
+export {default as tiramisu} from '../images/tiramisu.jpg';
