37ebc7e8acf0eb7d7bae65e54e21f39f4b7d5833
EnderXie23 <3142007828@qq.com>
2024-07-08 14:33:06 +0800

    Service Center modification

--- db-manager/src/main/scala/Global/ServiceCenter.scala
+++ db-manager/src/main/scala/Global/ServiceCenter.scala
@@ -5,7 +5,6 @@ import cats.effect.IO
 import com.comcast.ip4s.Port
 import org.http4s.Uri
 
-
 object ServiceCenter {
   val projectName: String = "APP"
 
@@ -13,47 +12,21 @@ object ServiceCenter {
   val chefServiceCode    = "A000002"
   val customerServiceCode   = "A000003"
   val portalServiceCode    = "A000004"
+  val adminServiceCode    = "A000005"
 
   val fullNameMap: Map[String, String] = Map(
     dbManagerServiceCode ->  "数据库管理（DB_Manager）",
     chefServiceCode    ->  "厨师（Chef）",
     customerServiceCode   ->  "顾客（Customer）",
-    portalServiceCode    ->  "门户（Portal）"
+    portalServiceCode    ->  "门户（Portal）",
+    adminServiceCode    ->  "管理员（Admin）"
   )
 
   val address: Map[String, String] = Map(
     "DB-Manager" ->     "127.0.0.1:10001",
     "Chef" ->         "127.0.0.1:10002",
     "Customer" ->        "127.0.0.1:10003",
-    "Portal" ->         "127.0.0.1:10004"
+    "Portal" ->         "127.0.0.1:10004",
+    "Admin" ->         "127.0.0.1:10005"
   )
-
-  def getURI(serviceCode: String): IO[Uri] =
-    IO.fromEither(Uri.fromString(
-      "http://localhost:" + getPort(serviceCode).value.toString + "/"
-    ))
-
-  def getPort(serviceCode: String): Port =
-    Port.fromInt(portMap(serviceCode)).getOrElse(
-      throw new IllegalArgumentException(s"Invalid port for serviceCode: $serviceCode")
-    )
-
-
-  def serviceName(serviceCode: String): String = {
-    val fullName = fullNameMap(serviceCode)
-    val start = fullName.indexOf("（")
-    val end = fullName.indexOf("）")
-    fullNameMap(serviceCode).substring(start + 1, end).toLowerCase
-  }
-
-  def portMap(serviceCode: String): Int = {
-    serviceCode.drop(1).toInt +
-      (if (serviceCode.head == 'A') 10000 else if (serviceCode.head == 'D') 20000 else 30000)
-  }
-
-
-  lazy val servicePort: Int = portMap(serviceCode)
-  lazy val serviceFullName: String = fullNameMap(serviceCode)
-  lazy val serviceShortName: String = serviceName(serviceCode)
-  lazy val schemaName: String = serviceName(serviceCode)
 }
