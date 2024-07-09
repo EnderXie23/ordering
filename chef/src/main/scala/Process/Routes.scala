28cee73c9091b3be3d6e1528ff1f51af8c557224
EnderXie23 <3142007828@qq.com>
2024-07-09 23:00:20 +0800

    chef does not handle admin message anymore

--- chef/src/main/scala/Process/Routes.scala
+++ chef/src/main/scala/Process/Routes.scala
@@ -14,11 +14,6 @@ import org.http4s.dsl.io.*
 object Routes:
   private def executePlan(messageType:String, str: String): IO[String]=
     messageType match {
-      case "AdminQueryMessage" =>
-        IO(decode[AdminQueryMessagePlanner](str).getOrElse(throw new Exception("Invalid JSON for AdminQueryMessage")))
-          .flatMap{m=>
-            m.fullPlan.map(_.asJson.toString)
-          }
       case "CompleteMessage" =>
         IO(decode[CompleteMessagePlanner](str).getOrElse(throw new Exception("Invalid JSON for CompleteMessage")))
           .flatMap{m=>
