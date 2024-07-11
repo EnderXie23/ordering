c1066e3a457a3980d5b7337a76de26f738af1be2
Quan Gu <cooolquan@163.com>
2024-07-11 16:39:43 +0800

    renderer: delete a useless import

--- login-panel/src/renderer.tsx
+++ login-panel/src/renderer.tsx
@@ -17,7 +17,6 @@ import OrderSummaryPage from 'Pages/CustomerPages/OrderSummaryPage'
 import CustomerFinishPage from 'Pages/CustomerPages/CustomerFinishPage'
 import OrderingMorePage from 'Pages/CustomerPages/OrderingMorePage'
 import CommentPage from 'Pages/CustomerPages/CommentPage'
-import Profile from 'Pages/CustomerPages/CustomerSidebar/Profile'
 
 const Layout = () => {
     return (
