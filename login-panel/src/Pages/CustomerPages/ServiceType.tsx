647a8a95f8181f9934baab67ef25eb7d8076c0dc
EnderXie23 <3142007828@qq.com>
2024-07-15 09:58:43 +0800

    ServiceType page UI beautify

--- login-panel/src/Pages/CustomerPages/ServiceType.tsx
+++ login-panel/src/Pages/CustomerPages/ServiceType.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useUser } from 'Pages/UserContext'
 import { useHistory } from 'react-router'
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, TextField, Button, Box, Typography } from '@mui/material';
 
 const ServiceType: React.FC = () => {
     const [serviceType, setServiceType] = useState<'dine-in' | 'takeaway'>('dine-in');
@@ -25,42 +26,34 @@ const ServiceType: React.FC = () => {
     };
 
     return (
-        <div>
-            <h1>请选择服务方式</h1>
-            <div>
-                <label>
-                    堂食
-                    <input
-                        type="radio"
-                        name="serviceType"
-                        value="dine-in"
-                        checked={serviceType === 'dine-in'}
-                        onChange={handleServiceTypeChange}
-                    />
-                </label>
-                <label>
-                    外卖
-                    <input
-                        type="radio"
-                        name="serviceType"
-                        value="takeaway"
-                        checked={serviceType === 'takeaway'}
-                        onChange={handleServiceTypeChange}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    {serviceType === 'dine-in' ? '桌号:' : '地址:'}
-                    <input
-                        type="text"
-                        value={inputValue}
-                        onChange={handleInputChange}
-                    />
-                </label>
-            </div>
-            <button onClick={handleSubmit}>确定</button>
-        </div>
+        <Box sx={{ maxWidth: 400, mx: 'auto', mt: 4, p: 3, border: '1px solid #ccc', borderRadius: 2 }}>
+            <Typography variant="h4" gutterBottom>请选择服务方式</Typography>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">服务方式</FormLabel>
+                <RadioGroup
+                    name="serviceType"
+                    value={serviceType}
+                    onChange={handleServiceTypeChange}
+                >
+                    <FormControlLabel value="dine-in" control={<Radio />} label="堂食" />
+                    <FormControlLabel value="takeaway" control={<Radio />} label="外卖" />
+                </RadioGroup>
+            </FormControl>
+            <TextField
+                label={serviceType === 'dine-in' ? '桌号' : '地址'}
+                value={inputValue}
+                onChange={handleInputChange}
+                fullWidth
+                autoFocus
+                margin="normal"
+            />
+            <Button variant="contained" color="primary" onClick={handleSubmit} fullWidth>
+                确定
+            </Button>
+            <Button variant="contained" color="secondary" onClick={() => {history.push('/')}} fullWidth sx={{marginTop: 2}}>
+                返回
+            </Button>
+        </Box>
     );
 };
 
