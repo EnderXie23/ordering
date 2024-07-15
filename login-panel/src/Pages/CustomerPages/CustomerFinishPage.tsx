04ea0b4be738f08597788596bd3ee188e0bd02f5
Quan Gu <cooolquan@163.com>
2024-07-15 22:04:59 +0800

    CustomerFinishPage: better UI

--- login-panel/src/Pages/CustomerPages/CustomerFinishPage.tsx
+++ login-panel/src/Pages/CustomerPages/CustomerFinishPage.tsx
@@ -2,30 +2,42 @@ import { useHistory } from 'react-router-dom';
 import React from 'react';
 import CustomerSidebar from './CustomerSidebar/CustomerSidebar'
 import { Container, Typography, Box, Button } from '@mui/material';
+import backgroundImage from 'Images/background.png'
 
 const CustomerFinishPage: React.FC = () => {
     const history = useHistory();
 
     return (
-        <Container>
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Typography variant="h4" gutterBottom>
-                    订单完成
-                </Typography>
-                <CustomerSidebar/>
-            </Box>
-            <Typography variant="body1" gutterBottom>
-                感谢您的订购！
-            </Typography>
-            <Box display="flex" justifyContent="center" alignItems="center" mt={4}>
-                <Button variant="contained" color="primary" onClick={() => { history.push('/comment') }} style={{ marginRight: '16px' }}>
-                    留下评论
-                </Button>
-                <Button variant="contained" color="primary" onClick={() => { history.push('/') }}>
-                    返回主页
-                </Button>
-            </Box>
-        </Container>
+        <div className='root' style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <Box className='cover' style={{ backgroundColor: 'rgba(255, 255, 255, 0.9)' }}/>
+            <Container style={{zIndex:2, width: '50%'}}>
+                <Box display="flex" flexDirection='column' justifyContent="center" alignItems="center">
+                    <Typography variant="h4" gutterBottom sx={{
+                        fontFamily: 'Noto Sans',
+                        fontSize: '2.5rem',
+                        fontWeight: 'bold',
+                        marginBottom: '2rem'
+                    }}>
+                        订单完成
+                    </Typography>
+                    <Typography variant="body1" gutterBottom sx={{
+                        fontFamily: 'Noto Sans',
+                        fontSize: '1rem',
+                    }}>
+                        感谢您的订购！
+                    </Typography>
+                </Box>
+                <Box display="flex" justifyContent="center" alignItems="center" mt={4}>
+                    <Button variant="contained" className='button' onClick={() => { history.push('/comment') }} style={{ marginRight: '16px' }}>
+                        留下评论
+                    </Button>
+                    <Button variant="contained" className='button' onClick={() => { history.push('/') }}>
+                        返回主页
+                    </Button>
+                </Box>
+            </Container>
+            <CustomerSidebar/>
+        </div>
     );
 };
 
